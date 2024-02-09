f4880a5cf42543f6672c5f03a4cd002536bf23a6
john <you@example.com>
2024-02-09 18:49:04 +0530

    index update

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const cors = require("cors");
 
 mongoose.connect(process.env.MONGO_URL);
 
+app.use(express.static(path.join(__dirname, "dist")));
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/admin", require("./routes/todoRoutes"));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  // res.status(404).json({ message: "Resource Not Found" });
+});
 
 mongoose.connection.once("open", () => {
   console.log("MONGO CONNECTED");
